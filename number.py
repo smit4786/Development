d28aa409415fe9446f01354da2cb25468a4a07e8
smit4786 <smit4786@gmail.com>
2023-12-13 11:04:02 -0500

    added loop break

--- number.py
+++ number.py
@@ -1,9 +1,12 @@
 ## Get integer for input
-try:
-    x = int(input("Define the value of x "))
+while True:
+    try:
+        x = int(input("Define the value of x "))
 ## Error Message if integer is not received
-except ValueError:
-    print("x is not an integer")
+    except ValueError:
+        print("x is not an integer")
 ## Print the value of x
-else:
-    print(f"x is {x}")
+    else:
+     break
+
+print(f"x is {x}")
