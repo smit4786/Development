21ec2e00f5ec5ed2ff386f670dd1047fe2cd2027
smit4786 <smit4786@gmail.com>
2023-12-13 11:21:29 -0500

    update to v.01

--- number.py
+++ number.py
@@ -1,12 +1,13 @@
+def main():
+    x = get_int()
+    print(f"x is {x}")
+def get_int():
 ## Get integer for input
-while True:
-    try:
-        x = int(input("Define the value of x "))
+    while True:
+        try:
+            return int(input("Define the value of x "))
 ## Error Message if integer is not received
-    except ValueError:
-        print("x is not an integer")
-## Print the value of x
-    else:
-     break
-
-print(f"x is {x}")
+        except ValueError:
+            print("x is not an integer")
+            pass
+main()
