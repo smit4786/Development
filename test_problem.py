a75baae997a507e6f355f06d50f2a0f50b322658
smit4786 <smit4786@gmail.com>
2023-12-14 13:17:41 -0500

    first draft

---
+++ test_problem.py
@@ -0,0 +1,20 @@
+""" 
+https://itunes.apple.com/search?parameterkeyvalue
+
+get 10 songs from each artist
+no explicit songs
+limit to 1 hour (get song length divisible by 60)
+random songs each time
+
+ """
+import random
+
+def main():
+   
+
+    random.shuffle(song_list)
+     return (song_list)
+
+    song_list = []
+       
+main()
